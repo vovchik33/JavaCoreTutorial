da211a3e8b6585bdf3ed5937c498891d14858032
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2014-11-20 21:32:27 +0200

    Добавлена задача на строки

---
+++ Lesson_0002/src/Task_0101.java
@@ -0,0 +1,13 @@
+/**
+ * Created by vladimirkr on 11/20/2014.
+ */
+public class Task_0101 {
+    public static void main(String[] args) {
+        int array[]={10,9,1,2,4,7,5,6,3,8};
+        for (int i = 0; i < array.length; i++) {
+            if (i%2==1) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
